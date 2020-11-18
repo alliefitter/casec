110de1a009333ee69b6406f9a94e39334e2633c0
Allie Fitter <allie.fitter@trucesoftware.com>
2020-11-18 15:36:07 -0600

    Added readme to package

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
 
+with open('readme.md', 'r') as f:
+    long_description = f.read()
+
 setup(
     name='casec',
-    version='0.2.0',
+    version='0.2.1',
     packages=find_packages(),
     include_package_data=True,
     entry_points={
@@ -10,9 +13,11 @@ setup(
             'casec = casec.main:main'
         ]
     },
-    url='',
+    url='https://github.com/alliefitter/casec',
     license='',
     author='Allie Fitter',
     author_email='afitter@cellcontrol.com',
     description='CLI that converts the case of strings.',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
